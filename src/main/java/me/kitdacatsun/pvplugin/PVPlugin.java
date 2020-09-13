d60d6260ddacf48a12c0d9be97222875728cd964
kit.matthewson@gmail.com <KitDaCatsun@users.noreply.github.com>
2020-09-13 11:47:06 +0100

    Progressed /start, Switched to Location()

--- src/main/java/me/kitdacatsun/pvplugin/PVPlugin.java
+++ src/main/java/me/kitdacatsun/pvplugin/PVPlugin.java
@@ -1,19 +1,29 @@
 package me.kitdacatsun.pvplugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PVPlugin extends JavaPlugin {
 
     public static Team[] teams;
+    public static Location[] spawnBarriers;
 
     @Override
     public void onEnable() {
         System.out.println("Started Up");
-
+        
+        World nether = Bukkit.getWorld("nether");
+        
         teams = new Team[] {
-                new Team("Red", -12.5f, 130, 0.5f),
-                new Team("Blue", 12.5f, 130, 0.5f),
-                new Team("None", 0.5f, 130, 0.5f)
+                new Team("Red", new Location(nether, -12.5f, 130, 0.5f)),
+                new Team("Blue", new Location(nether, 12.5f, 130, 0.5f)),
+                new Team("None", new Location(nether, 0.5f, 130, 0.5f))
+        };
+        
+        spawnBarriers = new Location[] {
+                
         };
 
         this.getCommand("join").setExecutor(new Join());
