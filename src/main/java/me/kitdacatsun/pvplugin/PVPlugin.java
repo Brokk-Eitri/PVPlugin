218136888a1abe2bf072757a44f2ac6237a89653
kit.matthewson@gmail.com <KitDaCatsun@users.noreply.github.com>
2020-09-13 11:35:01 +0100

    Progressed /join

--- src/main/java/me/kitdacatsun/pvplugin/PVPlugin.java
+++ src/main/java/me/kitdacatsun/pvplugin/PVPlugin.java
@@ -4,10 +4,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PVPlugin extends JavaPlugin {
 
+    public static Team[] teams;
+
     @Override
     public void onEnable() {
         System.out.println("Started Up");
 
+        teams = new Team[] {
+                new Team("Red", -12.5f, 130, 0.5f),
+                new Team("Blue", 12.5f, 130, 0.5f),
+                new Team("None", 0.5f, 130, 0.5f)
+        };
+
         this.getCommand("join").setExecutor(new Join());
     }
 }
+
+
