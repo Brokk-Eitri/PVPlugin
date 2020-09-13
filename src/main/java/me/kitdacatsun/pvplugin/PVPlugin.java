efc825e8d78e3f1ae1b567b50d6f0aa0e0766fdd
kit.matthewson@gmail.com <KitDaCatsun@users.noreply.github.com>
2020-09-13 12:20:00 +0100

    /start replaces blocks at positions in array. Registered /start

--- src/main/java/me/kitdacatsun/pvplugin/PVPlugin.java
+++ src/main/java/me/kitdacatsun/pvplugin/PVPlugin.java
@@ -2,6 +2,7 @@ package me.kitdacatsun.pvplugin;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -9,24 +10,29 @@ public final class PVPlugin extends JavaPlugin {
 
     public static Team[] teams;
     public static Location[] spawnBarriers;
+    public static Material spawnBarrierBlock;
 
+    @SuppressWarnings("ConstantConditions")
     @Override
     public void onEnable() {
         System.out.println("Started Up");
-        
+
         World nether = Bukkit.getWorld("nether");
-        
+
         teams = new Team[] {
                 new Team("Red", new Location(nether, -12.5f, 130, 0.5f)),
                 new Team("Blue", new Location(nether, 12.5f, 130, 0.5f)),
                 new Team("None", new Location(nether, 0.5f, 130, 0.5f))
         };
-        
+
         spawnBarriers = new Location[] {
-                
+
         };
 
+        spawnBarrierBlock  = Material.IRON_BARS;
+
         this.getCommand("join").setExecutor(new Join());
+        this.getCommand("start").setExecutor(new Start());
     }
 }
 
--- src/main/java/me/kitdacatsun/pvplugin/Start.java
+++ src/main/java/me/kitdacatsun/pvplugin/Start.java
@@ -1,6 +1,7 @@
 package me.kitdacatsun.pvplugin;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,7 +13,7 @@ public class Start implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         for (int i = 0; i < PVPlugin.spawnBarriers.length; i++) {
             Location location = PVPlugin.spawnBarriers[i];
-
+            location.getBlock().setType(Material.AIR);
         }
 
         return false;
