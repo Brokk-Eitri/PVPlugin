85837d7db253e23f3228c8b996ce788b40ccd7a6
kit.matthewson@gmail.com <KitDaCatsun@users.noreply.github.com>
2020-09-13 07:57:20 +0100

    Initial Commit

---
+++ src/main/java/me/kitdacatsun/pvplugin/PVPlugin.java
@@ -0,0 +1,16 @@
+package me.kitdacatsun.pvplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PVPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
